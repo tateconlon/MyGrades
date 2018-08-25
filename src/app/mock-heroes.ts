833117e6245881e485de0c690a51a18fd283c5e0
Tate Conlon <tate.conlon@gmail.com>
2018-08-25 13:16:35 -0400

    Half-way through tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Ameris'}, 
+	{ id: 12, name: 'Tate'}, 
+	{ id: 13, name: 'DJ Gravity'}, 
+	{ id: 14, name: 'Isaiah'}, 
+	{ id: 15, name: 'Mr. Nice'}, 
+	{ id: 16, name: 'Cool Doggo'}, 
+	{ id: 17, name: 'Big Sipp'}, 
+	];

749c8f2444014b6daf2d3bb7a0c4ad2bb25e7745
Ameris Rudland <13akar@queensu.ca>
2018-08-25 14:15:57 -0400

    slider test, added slider dependencies. Separate slider component will lkely be removed later

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatSliderModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatCheckboxModule,
     FormsModule
   ],
   providers: [],

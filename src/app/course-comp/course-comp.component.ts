b0638cd8e08cb3fd8ab4cc5ee43928274efba9c9
Ameris Rudland <13akar@queensu.ca>
2018-08-26 02:06:59 -0400

    commented code that might help backend load

--- src/app/course-comp/course-comp.component.ts
+++ src/app/course-comp/course-comp.component.ts
@@ -10,6 +10,18 @@ export class CourseCompComponent implements OnInit {
 
   course: Course = new Course();
 
+//const lib = require('lib');
+//
+// lib.username.bestTrekChar['@0.0.0']({name: 'spock'}, function (err, result) {
+//
+//   if (err) {
+//     // handle it
+//   }
+//
+//   // do something with result
+//
+// });
+
   recalculate():void {
     console.log('test');
     //iterate through the list and recalculate currentMark
